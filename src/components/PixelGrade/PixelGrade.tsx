1368a8c9a2a6e58e20108b762c38e6997194a559
Abderlahman-Omar <159623837+Abderlahman-Omar@users.noreply.github.com>
2025-06-29 16:42:02 +0300

    pixel grade section

--- src/components/PixelGrade/PixelGrade.tsx
+++ src/components/PixelGrade/PixelGrade.tsx
@@ -1,5 +1,16 @@
+import "./PixelGrade.css"
+import pixelImage from "../../assets/Pixel-Grade/Frame 35.svg"
 export default function PixelGrade() {
   return (
-    <div>PixelGrade</div>
+    <div className='pixel-container'>
+      <img src={pixelImage} alt="" />
+      <div className="pixel-text-container">
+        <p className='pixel-first-text'>The unseen of spending three years at Pixelgrade</p>
+        <p className='pixel-second-text'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sit amet justo ipsum. Sed accumsan quam vitae est varius fringilla. Pellentesque placerat vestibulum lorem sed porta. Nullam mattis tristique iaculis. Nullam pulvinar sit amet risus pretium auctor. Etiam quis massa pulvinar, aliquam quam vitae, tempus sem. Donec elementum pulvinar odio.</p>
+        <button>
+          Learn More
+        </button>
+      </div>
+    </div>  
   )
 }
--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -2,6 +2,7 @@ import Community from "../components/Community/Community";
 import Header from "../components/Header/Header";
 import Navbar from "../components/Navbar/Navbar";
 import OurClients from "../components/OurClients/OurClients";
+import PixelGrade from "../components/PixelGrade/PixelGrade";
 import './LandingPage.css'
 export default function LandingPage() {
   return (
@@ -10,6 +11,7 @@ export default function LandingPage() {
       <Header/>
       <OurClients/>
       <Community/>
+      <PixelGrade/>
     </div>
   )
 }
