5eae93f5164b62e3c9d59c40c056c8f6439e25cb
Abderlahman-Omar <159623837+Abderlahman-Omar@users.noreply.github.com>
2025-06-29 16:14:30 +0300

    adding our-clients section

--- src/components/OurClients/OurClients.tsx
+++ src/components/OurClients/OurClients.tsx
@@ -1,5 +1,23 @@
+import "./OurClients.css"
+import logo1 from "../../assets/Our-Clients/Logo (1).svg"
+import logo2 from "../../assets/Our-Clients/Logo (2).svg"
+import logo3 from "../../assets/Our-Clients/Logo (3).svg"
+import logo4 from "../../assets/Our-Clients/Logo (4).svg"
 export default function OurClients() {
   return (
-    <div>OurClients</div>
+    <div className='clients-container'>
+      <div className="clients-text-container">
+        <p className="clients-title">Our Clients</p>
+        <p className="clients-quote">we have been working with more than 500 clients</p>
+      </div>
+      <div className="clients-logo-section">
+        <img src={logo1} width={'50px'} alt="" />
+        <img src={logo2} width={'50px'} alt="" />
+        <img src={logo3} width={'50px'} alt="" />
+        <img src={logo4} width={'50px'} alt="" />
+        <img src={logo1} width={'50px'} alt="" />
+        <img src={logo2} width={'50px'} alt="" />
+      </div>
+    </div>
   )
 }
--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -1,11 +1,13 @@
 import Header from "../components/Header/Header";
 import Navbar from "../components/Navbar/Navbar";
+import OurClients from "../components/OurClients/OurClients";
 import './LandingPage.css'
 export default function LandingPage() {
   return (
     <div>
       <Navbar/>
       <Header/>
+      <OurClients/>
     </div>
   )
 }
